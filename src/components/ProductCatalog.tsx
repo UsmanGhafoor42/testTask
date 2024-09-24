597c442b89d8b4ca9bf9d013008d671662fd8a01
Usman Ghafoor <uk291244@gmail.com>
2024-09-24 07:20:23 -0700

    🚧 Work in progress.

--- src/components/ProductCatalog.tsx
+++ src/components/ProductCatalog.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { getProducts } from "../pages/api/get-products";
-import fetchApi from "../lib/strapi";
+import React from "react";
 
 interface Product {
   id: number;
@@ -9,31 +7,30 @@ interface Product {
   price: number;
 }
 
-export const ProductCatalog = ({products}: {products: any}) => {
+interface ProductCatalogProps {
+  products: Product[];
+}
 
-  // const fetchProucts = async () => {
-
-  //   const productData = await fetchApi({ endpoint: "products", wrappedByKey: "data" });
-  //   console.log("API response: ", productData);
-  // };
-
-  // fetchProucts();
+export const ProductCatalog = ({ products }: ProductCatalogProps) => {
   console.log("products: ", products);
 
   return (
     <div className="flex flex-wrap gap-4">
-      {products.map((product: any)=> (
-        <div className="flex items-center justify-center border rounded-md p-4 gap-10">
-         <img src="http://localhost:1337/uploads/Macbook_ad3a4877cd.jpeg" alt={product.Title}  width={400} height={400}/>
-         <div className="flex flex-col items-center justify-center gap-5">
-
-         <h1 className="text-2xl font-bold">{product.Title}</h1>
-         <p className="text-xl"> Price: {product.Price}</p>
-         </div>
-         <button className="bg-bla text-white px-4 py-2 rounded-md">Add to Cart</button>
+      {products.map((product) => (
+        <div key={product.id} className="flex items-center justify-center border rounded-md p-4 gap-10">
+          <img
+            src={`http://localhost:1337${product.image.url}`}
+            alt={product.title}
+            width={400}
+            height={400}
+          />
+          <div className="flex flex-col items-center justify-center gap-5">
+            <h1 className="text-2xl font-bold">{product.title}</h1>
+            <p className="text-xl">Price: {product.price}</p>
+          </div>
+          <button className="bg-black text-white px-4 py-2 rounded-md">Add to Cart</button>
         </div>
       ))}
-
     </div>
   );
 };
